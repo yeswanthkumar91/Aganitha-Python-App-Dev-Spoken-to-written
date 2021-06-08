8a991a20ff0d941e81d4b98cee3bf5cdd4a7d8d0
yeswanthkumar91 <85543174+yeswanthkumar91@users.noreply.github.com>
2021-06-08 11:53:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def rules():
     return rules
 
 #checking if word has comma at front or at last or at both  if true then return front,word and last 
-def check_front_last(word):
+def check_for_Coma(word):
     front=""
     last=""
     if(len(word)>1):
@@ -63,20 +63,19 @@ class spoken:
 
         self.rules=rules()
         self.paragraph=""
-        self.ouptut_para=""
+        self.ouptut_paragraph=""
 
-    #getting user input
-    def get_user_input(self):
+    #user input
+    def user_input(self):
 
-        self.paragraph=input("\n[IN]:Enter Your paragraph of spoken english:\n\t")
+        self.paragraph=input("Enter input")
 
         if not self.paragraph:
-            raise ValueError("[Error]: You entered nothing.")
+            raise ValueError("Wrong Data")
 
-    #getting  user output
-    def show_output(self):
-        print("\n[OUT]:The input Spoken English Paragraph: \n\n \" "+ self.paragraph+"\"")
-        print("\nConverted Written English Paragraph: \n\n \"" +self.ouptut_para+"\"")
+    #output
+    def print_output(self):
+        print(self.ouptut_paragraph)
 
     
     #main conversion function of spoken to written english 
@@ -93,39 +92,39 @@ class spoken:
         #loop will run for the number of words in paragraph 
         while i<no_of_words: 
             
-            front,word,last=check_front_last(words_of_para[i])
+            front,word,last=check_for_Coma(words_of_para[i])
             #Word of paragraph may of form ',dollars.' 
             if i+1!= no_of_words:
             #when word is of the form e.g.: two 
-                front_n,next_word,last_n=check_front_last(words_of_para[i+1])
+                front_n,next_word,last_n=check_for_Coma(words_of_para[i+1])
                 if word.lower() in numbers.keys() and (next_word.lower()=='dollars' or next_word.lower()=='dollar'):
-                    self.ouptut_para=self.ouptut_para+" "+front+"$"+str(numbers[word.lower()])+last
+                    self.ouptut_paragraph=self.ouptut_paragraph+" "+front+"$"+str(numbers[word.lower()])+last
                     i=i+2
 
                 elif word.lower() in tuples.keys() and len(next_word)==1:
                     #when word is of form Triple A
-                    self.ouptut_para=self.ouptut_para+" "+front_n+(next_word*tuples[word.lower()])+last_n
+                    self.ouptut_paragraph=self.ouptut_paragraph+" "+front_n+(next_word*tuples[word.lower()])+last_n
                     i=i+2
                 elif (word+" "+next_word) in general.keys():
                     #if word is of form P M or C M
-                    self.ouptut_para=self.ouptut_para+" "+front+word+next_word+last_n
+                    self.ouptut_paragraph=self.ouptut_paragraph+" "+front+word+next_word+last_n
                     i=i+2
                 else:
-                    self.ouptut_para=self.ouptut_para+" "+words_of_para[i]
+                    self.ouptut_paragraph=self.ouptut_paragraph+" "+words_of_para[i]
                     i=i+1
             else:
-                self.ouptut_para=self.ouptut_para+" "+words_of_para[i]
+                self.ouptut_paragraph=self.ouptut_paragraph+" "+words_of_para[i]
                 i=i+1
 
 
 
 def main():
     obj=spoken()
-    obj.get_user_input()
+    obj.user_input()
     obj.ConvertToWritten()
 
 
-    obj.show_output()
+    obj.print_output()
 
 
 #main function 
